983ac9d2f2926ce911a953cb7bf751918190f193
Yusioka <lilacdeer1205@gmail.com>
2023-10-09 16:20:25 +0300

    Shop System Update

---
+++ Assets/Scripts/Shops/Shop.cs
@@ -0,0 +1,30 @@
+using RPG.Inventories;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Shops
+{
+    public class Shop : MonoBehaviour
+    {
+        public class ShopItem
+        {
+            InventoryItem item;
+            int availability;
+            float price;
+            int quantityInTransaction;
+        }
+
+        public event Action onChange;
+
+        public IEnumerable<ShopItem> GetFilteredItems() { return null; }
+        public void SelectCategory(ItemCategory category) { }
+        public ItemCategory GetFilter() { return ItemCategory.None; }
+        public void SelectMode(bool isBuying) { }
+        public bool IsBuyingMode() { return true;}
+        public bool CanTransact() { return true; }
+        public void ConfirmTransaction() { }
+        public float TransactionTotal() { return 0; }
+        public void AddToTransaction(InventoryItem item, int quantity) { }
+    }
+}
