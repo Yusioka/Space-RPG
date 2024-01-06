4864f3e6eea50ce70acbe966c85d4890c7109196
Yusioka <lilacdeer1205@gmail.com>
2024-01-06 20:59:31 +0200

    RandomDropper Added

---
+++ Assets/Scripts/Inventories/DropLibrary.cs
@@ -0,0 +1,115 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Inventories
+{
+    [CreateAssetMenu(fileName = "New Drop Library", menuName = "Inventory/New Drop Library", order = 0)]
+    public class DropLibrary : ScriptableObject
+    {
+        [SerializeField] DropConfig[] potentialDrops;
+        [SerializeField] float[] dropChancePercentage;
+        [SerializeField] int[] minDrops;
+        [SerializeField] int[] maxDrops;
+
+        [System.Serializable]
+        class DropConfig
+        {
+            public InventoryItem item;
+            public float[] relativeChance;
+            public int[] minNumber;
+            public int[] maxNumber;
+            public int GetRandomNumber(int level)
+            {
+                if (!item.IsStackable())
+                {
+                    return 1;
+                }
+                int min = GetByLevel(minNumber, level);
+                int max = GetByLevel(maxNumber, level);
+                return Random.Range(min, max + 1);
+            }
+        }
+
+        public struct Dropped
+        {
+            public InventoryItem item;
+            public int number;
+        }
+
+        public IEnumerable<Dropped> GetRandomDrops(int level)
+        {
+            if (!ShouldRandomDrop(level))
+            {
+                yield break;
+            }
+            for (int i = 0; i < GetRandomNumberOfDrops(level); i++)
+            {
+                yield return GetRandomDrop(level);
+            }
+        }
+
+        private bool ShouldRandomDrop(int level)
+        {
+            return Random.Range(0, 100) < GetByLevel(dropChancePercentage, level);
+        }
+
+        private int GetRandomNumberOfDrops(int level)
+        {
+            int min = GetByLevel(minDrops, level);
+            int max = GetByLevel(maxDrops, level);
+            return Random.Range(min, max);
+        }
+
+        private Dropped GetRandomDrop(int level)
+        {
+            var drop = SelectRandomItem(level);
+            var result = new Dropped();
+            result.item = drop.item;
+            result.number = drop.GetRandomNumber(level);
+            return result;
+        }
+
+        private DropConfig SelectRandomItem(int level)
+        {
+            float totalChance = GetTotalChance(level);
+            float randomRoll = Random.Range(0, totalChance);
+            float chanceTotal = 0;
+            foreach (var drop in potentialDrops)
+            {
+                chanceTotal += GetByLevel(drop.relativeChance, level);
+                if (chanceTotal > randomRoll)
+                {
+                    return drop;
+                }
+            }
+            return null;
+        }
+
+        private float GetTotalChance(int level)
+        {
+            float total = 0;
+            foreach (var drop in potentialDrops)
+            {
+                total += GetByLevel(drop.relativeChance, level);
+            }
+            return total;
+        }
+
+        static T GetByLevel<T>(T[] values, int level)
+        {
+            if (values.Length == 0)
+            {
+                return default;
+            }
+            if (level > values.Length)
+            {
+                return values[values.Length - 1];
+            }
+            if (level <= 0)
+            {
+                return default;
+            }
+            return values[level - 1];
+        }
+    }
+}
